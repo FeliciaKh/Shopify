a092944b7440f99f4ef197392a29f7f610269b0e
FeliciaKh <feliciakh0428@gmail.com>
2020-09-06 14:30:41 -0400

    Tester

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <h1>HELLO WORLD</h1>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload and test this.
         </p>
+        <h1>Hello World!</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,9 +20,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
-  );
+  )
 }
 
 export default App;
